df07260f14ddc55948176dcc20030e506e564769
karan <karanc4143@gmail.com>
2025-12-01 18:46:19 +0530

    basic functinality done

---
+++ Youtub Manager App/Main.py
@@ -0,0 +1,58 @@
+import sys
+
+def list_all_videos(videos):
+    print(f'this is file {videos}')
+
+def add_video(videos):
+    raise NotImplementedError
+
+def delete_video(videos):
+    raise NotImplementedError
+
+def update_video(videos):
+    raise NotImplementedError
+
+def load_data():
+    try:
+        with  open('data.txt' ,'a+') as file:
+            file.write("hellow python")
+            file.seek(0)
+            return  file.read()
+    except FileNotFoundError:
+        return []
+    except  Exception as e:
+        print(f'Error: {e}')
+    
+
+
+def main():
+    videos=load_data()
+    
+    user = sys.argv[1] if len(sys.argv)>1 else "Guest"
+    print(f'Hellow  {user}')
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
+         
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                delete_video(videos)
+            case '4':
+                update_video(videos)
+            case '5':
+                break
+            case _:
+                print("sorry ! invalid Input please try again latter")
+
+if __name__=='__main__':
+    main()
